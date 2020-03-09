2d37825f99998b5d9a0ae133bf25d57ede7f0206
Andres Felipe Gonzalez Rojas <pipesoft2010@gmail.com>
2020-03-09 11:02:57 -0500

    Cambios de nombre de la Solucion

--- resources/views/publico.blade.php
+++ resources/views/publico.blade.php
@@ -9,7 +9,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'Sistema de Gestión Contractual') }}</title>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
